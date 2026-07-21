ddc3846eec7b1350eee06b7be29f5089f52d610c
Luan Oliveira <luanoliveira4034e@gmail.com>
2026-07-21 15:47:07 -0300

    Update firebase.js

--- script/firebase.js
+++ script/firebase.js
@@ -1,21 +1,17 @@
-// js/firebase.js
+// script/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "SEU_AUTH_DOMAIN_AQUI",
-    projectId: "SEU_PROJECT_ID_AQUI",
-    storageBucket: "SEU_STORAGE_BUCKET_AQUI",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID_AQUI",
-    appId: "SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyBH4hDVSqv7enMugPWUaM9Cks04F1yKvuQ",
+  authDomain: "controle-de-validade-3e66a.firebaseapp.com",
+  projectId: "controle-de-validade-3e66a",
+  storageBucket: "controle-de-validade-3e66a.firebasestorage.app",
+  messagingSenderId: "163605465156",
+  appId: "1:163605465156:web:8f7728cbf73e6bf6265411"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta as instâncias para os outros módulos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
