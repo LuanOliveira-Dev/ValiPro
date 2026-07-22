3d4279639f3d0a43a86b25996e6d24efdf5673b1
Luan Oliveira <luanoliveira4034e@gmail.com>
2026-07-22 14:06:03 -0300

    Update firebase.js

--- script/firebase.js
+++ script/firebase.js
@@ -4,15 +4,18 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // Verifique se a chave abaixo não tem nenhum caractere faltando
-  apiKey: "AIzaSyBH4hDVSqv7enMugPWUaM9Cks04F1yKvuQ", 
+  apiKey: "AIzaSyBH4hDVSqv7enMugPWUaM9Cks04F1yKvuQ",
   authDomain: "controle-de-validade-3e66a.firebaseapp.com",
   projectId: "controle-de-validade-3e66a",
   storageBucket: "controle-de-validade-3e66a.firebasestorage.app",
   messagingSenderId: "163605465156",
-  appId: "1:163605465156:web:8f7728cbf73e6bf6265411"
+  appId: "1:163605465156:web:8f7728cbf73e6bf6265411",
+  measurementId: "G-EVLB6HPR2F"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta as instâncias necessárias para auth.js, app.js, etc.
 export const auth = getAuth(app);
 export const db = getFirestore(app);
