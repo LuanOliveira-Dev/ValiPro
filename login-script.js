9616afd8e83be384998e09d608b88b7281031292
Luan Oliveira <luanoliveira4034e@gmail.com>
2026-07-22 13:53:56 -0300

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -1,5 +1,5 @@
 // Importa o serviço de autenticação do seu arquivo central
-import { auth } from "./firebase.js";
+import { auth } from "./script/firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 document.addEventListener('DOMContentLoaded', () => {
